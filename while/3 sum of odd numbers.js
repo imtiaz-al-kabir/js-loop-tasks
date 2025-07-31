c44dad4a17a15c73a6b1df5607a3ad8e35e08596
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-07-31 16:47:21 +0600

    while loop task done

--- while/3 sum of odd numbers.js
+++ while/3 sum of odd numbers.js
@@ -1,5 +1,3 @@
-
-
 /***
 
 Subtask-1:
@@ -15,4 +13,23 @@ Display sum of all the even numbers from 206 to 311.
 
  */
 
+let i = 81;
+let sum = 0;
+while (i <= 131) {
+  if (i % 2 != 0) {
+    sum += i;
+  }
+  i++;
+}
+console.log(sum);
 /*programming hero*/
+
+let j = 206;
+let sum1 = 0;
+while (j <= 311) {
+  if (j % 2 == 0) {
+    sum1 += j;
+  }
+  j++;
+}
+console.log(sum1);
--- while/4 multiplication table.js
+++ while/4 multiplication table.js
@@ -3,6 +3,12 @@
 As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
 
  */
-
+let i = 1;
+let num = 9;
+while (i <= 10) {
+  const result = num * i;
+  console.log(`${num} X ${i} = ${result}`);
+  i++;
+}
 
 /*programming hero*/
