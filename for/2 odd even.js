f2faf520107b97d6cd8cdb39a57dc9c046d42156
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-07-31 16:13:01 +0600

    1 2 task completed in for loop

--- for/2 odd even.js
+++ for/2 odd even.js
@@ -5,7 +5,11 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
-
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
 
 /***
 
@@ -14,5 +18,11 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
+
+for (let j = 78; j <= 98; j++) {
+  if (j % 2 === 0) {
+    console.log(j);
+  }
+}
 
 /*programming hero*/
